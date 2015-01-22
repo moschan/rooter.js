7a9a4f4f290f0e51493dad761539cb68481a000e
moschan <kaityo02@gmail.com>
2015-01-22 22:18:00 +0900

    initial commit

---
+++ rooter.js
@@ -0,0 +1,103 @@
+/**
+*
+*  Rooter
+*
+*/
+var Router = {
+	routes: [],
+	root: '/',
+
+	// setting root
+	config: function( root_path ) {
+		this.root = root_path || '/';
+		return this;
+	},
+
+	// add routing
+	add: function(re, handler) {
+		this.routes.push({ re: re, handler: handler});
+		return this;
+	},
+
+	// delete param
+	remove: function(param) {
+		for(var i=0; i<this.routes.length; i++) {
+			if(r.handler === param || r.re.toString() === param.toString()) {
+				this.routes.splice(i, 1);
+				return this;
+			}
+		}
+		return this;
+	},
+
+	// check rooting
+	check: function() {
+		var path = location.pathname;
+		path = this.root != '/' ? path.replace(this.root, '') : '/';
+		for(var i = 0; i < this.routes.length; ++i) {
+			var func = this.routes[i];
+			var match = path.match(func.re);
+			if ( match ) { func.handler(match); }
+		}
+		return this;
+	},
+
+	// initialize
+	initilize: function() {
+		this.routes = [];
+		return this;
+	}
+};
+
+
+/**
+*
+*  functions
+*
+*/
+(function() {
+
+	/**
+	*
+	*  dummy function
+	*
+	*/
+	function Hoge() {
+		"use strict";
+		this.hoge = function() {
+		};
+	}
+
+
+	/**
+	*
+	*  common function
+	*
+	*/
+	function common() {
+
+	}
+	// global
+	window.Validator = new Validator();
+	window.common = common;
+
+})();
+
+$(document).ready(function() {
+
+	// initialize
+	Router.initilize();
+	Router.config('/');
+
+	// add routing
+	Router
+	.add(/something\/page/, function() {
+	})
+	.add('', function() {
+		// common action
+	});
+
+	// Fire!!!
+	Router.check();
+
+});
